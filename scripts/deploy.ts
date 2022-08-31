42f743eda3a10ce0efbc7f2307589369ca662295
Ankush263 <ankushbanik263@gmail.com>
2022-08-31 17:56:50 +0530

    update

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,13 +1,18 @@
-import { ethers } from "hardhat";
+// import { hre } from 'hardhat'
+import ethers from "@nomiclabs/hardhat-ethers"
+const hre = require("hardhat")
 
 async function main() {
 
-  const ANKToken = await ethers.getContractFactory("TokenSwap");
+  const ANKToken = await hre.ethers.getContractFactory("TokenSwap");
   const ankToken = await ANKToken.deploy();
 
   await ankToken.deployed();
 
   console.log(`ANKToken deployed to ${ankToken.address}`);
+
+  // console.log(ethers)
+  // console.log(hre.ethers)
 }
 
 // We recommend this pattern to be able to use async/await everywhere
@@ -16,3 +21,4 @@ main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
+
