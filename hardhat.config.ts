7f44ecab26dbd529e61835110a13d31720e67201
Ankush263 <ankushbanik263@gmail.com>
2022-08-31 17:54:03 +0530

    update config file

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,15 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+require('@nomiclabs/hardhat-waffle')
+require('dotenv').config()
 
-const config: HardhatUserConfig = {
-  solidity: "0.8.9",
-};
 
-export default config;
+module.exports = {
+  solidity: '0.8.0',
+  networks: {
+    goerli: {
+      url: `https://goerli.infura.io/v3/${process.env.API_KEY}`,
+      accounts: [
+        `${process.env.PRIVATE_KEY}`,
+      ],
+    },
+  },
+}
